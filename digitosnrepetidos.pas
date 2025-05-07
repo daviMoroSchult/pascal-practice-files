fa1996c27f0fd79cb258b0b7bc79f8872838af22
daviMoroSchult <158090477+daviMoroSchult@users.noreply.github.com>
2025-05-06 22:30:31 -0300

    Add files via upload

---
+++ digitosnrepetidos.pas
@@ -0,0 +1,16 @@
+program n4drn;
+
+var n, i, tests, resposta: integer;
+
+begin
+
+read(n);
+
+if (n div 1000) <> (n div 100) and (n div 1000) <> (n div 10) and ( n <> ffmod 10 and n div 100 <> n div 10 and n div 100 <> n mod 10 and n div 10 <> (n mod 10)
+  then writeln('oi');
+while n < 1023 and n function do
+ begin
+
+ end
+
+end.                                                                            fff
